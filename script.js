941d045cec6b448f97e4cd31293d2dc284db6150
prashantsde11 <prashantsde11@gmail.com>
2025-04-23 07:43:00 +0530

    Javascript and html updated code

---
+++ script.js
@@ -0,0 +1,27 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove('active');
+  });
+  slides[index].classList.add('active');
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
+
+function toggleMenu() {
+  document.querySelector('.header-right').classList.toggle('active');
+}
+
+
+
+setInterval(nextSlide, 3000); // Auto-slide every 3 seconds
