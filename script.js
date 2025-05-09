b0d58ecd77266e82d8443cef17754f43a9a6ff51
Prashant Mishra <prashantsde11@gmail.com>
2025-05-09 08:35:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,18 +2,20 @@ let currentSlide = 0;
 const slides = document.querySelectorAll('.slide');
 const totalSlides = slides.length;
 
-const showSlide = index => {
+const showSlide = (index) => {
   slides.forEach(slide => slide.classList.remove('active'));
   slides[index].classList.add('active');
 };
 
-const changeSlide = step => {
-  currentSlide = (currentSlide + step + totalSlides) % totalSlides;
+const nextSlide = () => {
+  currentSlide = (currentSlide + 1) % totalSlides;
   showSlide(currentSlide);
 };
 
-const nextSlide = () => changeSlide(1);
-const prevSlide = () => changeSlide(-1);
+const prevSlide = () => {
+  currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+  showSlide(currentSlide);
+};
 
 const toggleMenu = () => {
   document.querySelector('.header-right').classList.toggle('active');
